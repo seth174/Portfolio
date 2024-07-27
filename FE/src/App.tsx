ff993cd29abf2f38017782ebe0394046bbdb0316
seth174 <sethfagen22@gmail.com>
2024-07-27 11:49:39 -0700

    Added fe folder

---
+++ FE/src/App.tsx
@@ -0,0 +1,45 @@
+// src/App.tsx
+import React, { useState } from 'react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { lightTheme, darkTheme } from './theme';
+import Button from '@mui/material/Button';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import DrawerAppBar from './components/NavBar';
+
+const App: React.FC = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  return (
+    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+
+      <Router>
+        <CssBaseline />
+        <DrawerAppBar checked={isDarkMode} setChecked={setIsDarkMode} />
+        <div style={{ marginTop: '70px' }}>
+          <Button variant="contained" color="primary" onClick={toggleTheme}>
+            Toggle Theme
+          </Button>
+          <Button variant="contained" color="secondary">
+            Secondary Button
+          </Button>
+        </div>
+        <div>
+          <Routes>
+            <Route path="/" Component={Home} />
+            <Route path="/about" Component={About} />
+            <Route path="/contact" Component={Contact} />
+          </Routes>
+        </div>
+      </Router>
+    </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ FE/src/pages/About.tsx
@@ -0,0 +1,8 @@
+// components/About.tsx
+import React from 'react';
+
+const About: React.FC = () => {
+    return <h2>About</h2>;
+}
+
+export default About;
