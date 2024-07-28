a9c5dc8d702eac86fd1ae0a214a475064c4bc5f8
seth174 <sethfagen22@gmail.com>
2024-07-28 11:09:28 -0700

    Try to get stuff from cosmo db

--- BE/src/functions/project.ts
+++ BE/src/functions/project.ts
@@ -1,4 +1,22 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { app, HttpRequest, HttpResponseInit, input, InvocationContext } from "@azure/functions";
+
+const cosmosInput = input.cosmosDB({
+    databaseName: 'portfolio',
+    collectionName: 'Projects',
+    id: '4962025d-98b2-4dd0-8110-3638cd0a5f43',
+    partitionKey: 'b6697ec8-79cd-4d6a-ad33-e2ffcfbe191d',
+    connectionStringSetting: 'CosmosDBConnection',
+});
+
+interface Project {
+    id: string;
+    projectId: string;
+    languages: string;
+    frameworks: string;
+    title: string;
+    description: string;
+    imageURL: string;
+}
 
 export async function GetProjects(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
@@ -7,6 +25,20 @@ export async function GetProjects(request: HttpRequest, context: InvocationConte
 
     return { body: `Hello, ${name}! integrated` };
 };
+
+export async function httpTrigger1(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    const project = <Project>context.extraInputs.get(cosmosInput);
+    if (!project) {
+        return {
+            status: 404,
+            body: 'project item not found',
+        };
+    } else {
+        return {
+            body: JSON.stringify(project),
+        };
+    }
+}
 
 app.http('GetProjects', {
     methods: ['GET', 'POST'],
@@ -18,4 +50,11 @@ app.http('GetProjects2', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: GetProjects
+});
+
+app.http('httpTrigger1', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    extraInputs: [cosmosInput],
+    handler: httpTrigger1,
 });
